4b12a426038cf4897e5ba21092cf8953072dedee
Jose Algarin <89646889+Joss-ai@users.noreply.github.com>
2023-03-24 17:32:54 -0500

    Add files via upload

--- Laboratorio 1/Laboratiorio_comu1_1.py
+++ Laboratorio 1/Laboratiorio_comu1_1.py
@@ -26,6 +26,7 @@ from gnuradio.filter import firdes
 import sip
 from gnuradio import analog
 from gnuradio import blocks
+from gnuradio import filter
 from gnuradio import gr
 from gnuradio.fft import window
 import sys
@@ -33,6 +34,9 @@ import signal
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
+from gnuradio.qtgui import Range, RangeWidget
+from PyQt5 import QtCore
+import E3TRadio
 
 
 
@@ -75,13 +79,20 @@ class Laboratiorio_comu1_1(gr.top_block, Qt.QWidget):
         # Variables
         ##################################################
         self.samp_rate = samp_rate = 32000
-        self.frequency = frequency = 100
+        self.frequency = frequency = 1000
+        self.Amplitud = Amplitud = 1
 
         ##################################################
         # Blocks
         ##################################################
 
-        self.qtgui_time_sink_x_0 = qtgui.time_sink_c(
+        self._frequency_range = Range(1000, 2000, 100, 1000, 200)
+        self._frequency_win = RangeWidget(self._frequency_range, self.set_frequency, "'frequency'", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._frequency_win)
+        self._Amplitud_range = Range(0, 1, 0.1, 1, 200)
+        self._Amplitud_win = RangeWidget(self._Amplitud_range, self.set_Amplitud, "'Amplitud'", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._Amplitud_win)
+        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
             1024, #size
             samp_rate, #samp_rate
             "", #name
@@ -116,12 +127,9 @@ class Laboratiorio_comu1_1(gr.top_block, Qt.QWidget):
             -1, -1, -1, -1, -1]
 
 
-        for i in range(2):
+        for i in range(1):
             if len(labels[i]) == 0:
-                if (i % 2 == 0):
-                    self.qtgui_time_sink_x_0.set_line_label(i, "Re{{Data {0}}}".format(i/2))
-                else:
-                    self.qtgui_time_sink_x_0.set_line_label(i, "Im{{Data {0}}}".format(i/2))
+                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
             else:
                 self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
             self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
@@ -132,7 +140,7 @@ class Laboratiorio_comu1_1(gr.top_block, Qt.QWidget):
 
         self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
-        self.qtgui_freq_sink_x_0 = qtgui.freq_sink_c(
+        self.qtgui_freq_sink_x_0 = qtgui.freq_sink_f(
             1024, #size
             window.WIN_BLACKMAN_hARRIS, #wintype
             0, #fc
@@ -153,6 +161,7 @@ class Laboratiorio_comu1_1(gr.top_block, Qt.QWidget):
         self.qtgui_freq_sink_x_0.set_fft_window_normalized(False)
 
 
+        self.qtgui_freq_sink_x_0.set_plot_pos_half(not True)
 
         labels = ['', '', '', '', '',
             '', '', '', '', '']
@@ -174,14 +183,31 @@ class Laboratiorio_comu1_1(gr.top_block, Qt.QWidget):
 
         self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_freq_sink_x_0_win)
-        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
-        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_SIN_WAVE, frequency, 1, 0, 0)
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_float*1, samp_rate,True)
+        self.blocks_add_xx_0 = blocks.add_vff(1)
+        self.band_pass_filter_0 = filter.interp_fir_filter_fff(
+            1,
+            firdes.band_pass(
+                1,
+                samp_rate,
+                1500,
+                1900,
+                100,
+                window.WIN_HAMMING,
+                6.76))
+        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_SIN_WAVE, frequency, Amplitud, 0, 0)
+        self.analog_noise_source_x_0 = analog.noise_source_f(analog.GR_GAUSSIAN, 0.5, 0)
+        self.E3TRadio_amplificador_ff_0 = E3TRadio.amplificador_ff(5)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_throttle_0, 0))
+        self.connect((self.E3TRadio_amplificador_ff_0, 0), (self.band_pass_filter_0, 0))
+        self.connect((self.analog_noise_source_x_0, 0), (self.blocks_add_xx_0, 1))
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_add_xx_0, 0))
+        self.connect((self.band_pass_filter_0, 0), (self.blocks_throttle_0, 0))
+        self.connect((self.blocks_add_xx_0, 0), (self.E3TRadio_amplificador_ff_0, 0))
         self.connect((self.blocks_throttle_0, 0), (self.qtgui_freq_sink_x_0, 0))
         self.connect((self.blocks_throttle_0, 0), (self.qtgui_time_sink_x_0, 0))
 
@@ -200,6 +226,7 @@ class Laboratiorio_comu1_1(gr.top_block, Qt.QWidget):
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
+        self.band_pass_filter_0.set_taps(firdes.band_pass(1, self.samp_rate, 1500, 1900, 100, window.WIN_HAMMING, 6.76))
         self.blocks_throttle_0.set_sample_rate(self.samp_rate)
         self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate)
         self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
@@ -210,6 +237,13 @@ class Laboratiorio_comu1_1(gr.top_block, Qt.QWidget):
     def set_frequency(self, frequency):
         self.frequency = frequency
         self.analog_sig_source_x_0.set_frequency(self.frequency)
+
+    def get_Amplitud(self):
+        return self.Amplitud
+
+    def set_Amplitud(self, Amplitud):
+        self.Amplitud = Amplitud
+        self.analog_sig_source_x_0.set_amplitude(self.Amplitud)
 
 
 
